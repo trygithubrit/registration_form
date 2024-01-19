74e4f30be0d4efd8e177790b8014315939cf0895
Amit <Amit@code2306.com>
2024-01-19 21:50:49 +0530

    some message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static('docs'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/Database')
